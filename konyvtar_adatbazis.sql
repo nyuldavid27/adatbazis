cd2679d0c313da60c760218904cc63650ed3b997
Nyúl Dávid <nyul.david@diak.szbi-pg.hu>
2025-11-20 14:19:36 +0100

    Create konyvtar_adatbazis.sql

---
+++ konyvtar_adatbazis.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE konyvtaradatbazis
+    CHARACTER SET utf8mb4
+    COLLATE utf8mb4_hungarian_ci;
+
+-- 3. feladat
+
+USE konyvtaradatbazis;
+
+-- 4. feladat 
+
+CREATE TABLE konyvek (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    cim VARCHAR(200) NOT NULL UNIQUE,
+    szerzo VARCHAR(120),
+    kiadasi_ev YEAR, 
+    oldal_szam INTEGER CHECK(BETWEEN 20 AND 2000),
+    kategoria ENUM("fantasy", "sci-fi", "disztópia", "történelem"),
+    ertekeles FLOAT DEFAULT 5.0
+);
+
+-- 5. feladat
+
+CREATE Table olvasok(
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nev VARCHAR(100) NOT NULL ,
+    szuletesi-datum DATE,
+    varos VARCHAR(50) DEFAULT Budapest
+    
+)
